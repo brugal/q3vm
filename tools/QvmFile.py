7b431de6668d3597a757bb9fa44bebea49686c57
Angelo Cano <acano@fastmail.fm>
2020-01-10 03:43:41 -0500

    copyright

--- tools/QvmFile.py
+++ tools/QvmFile.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2012, 2020 Angelo Cano
+#
+# This file is part of Qvmdis.
+#
+# Qvmdis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Qvmdis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Qvmdis.  If not, see <https://www.gnu.org/licenses/>.
+
 # allow importing from parent directory
 # https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
 #  answered Jun 22 '12 at 14:30  Remi
